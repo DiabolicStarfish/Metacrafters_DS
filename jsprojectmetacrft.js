e5d3a035356656a738c71f5627865676e8712823
Charles Andrew Santos <60927043+DiabolicStarfish@users.noreply.github.com>
2023-09-22 12:36:32 +0800

    Add files via upload

---
+++ jsprojectmetacrft.js
@@ -0,0 +1,50 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const holdNFT = ["happyOgre", "sadEmu", "angryApe"];
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (referenceId,nftName,index) {
+    holdNFT[index] = {
+        id : referenceId,
+        name: nftName
+    };
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    for(let i = 0; i<holdNFT.length; i++){
+        console.log(holdNFT[i].id);
+        console.log(holdNFT[i].name);
+    }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    if(holdNFT[0].id == null || holdNFT[0].name == null){
+        console.log("There are no minted NFTs.");
+    }else{
+        for(let i = 0; i<holdNFT.length; i++){
+            console.log(holdNFT[i]);
+        }
+    }
+}
+
+// call your functions below this line
+mintNFT("00", "George",0);
+mintNFT("01", "John",1);
+mintNFT("02", "Bob",2);
+
+listNFTs();
+
+getTotalSupply();
