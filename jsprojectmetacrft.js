31a79354491652770f727dc125645b70cdef18e6
Charles Andrew Santos <60927043+DiabolicStarfish@users.noreply.github.com>
2023-09-24 10:38:04 +0800

    jsprojectmetacrft.js

--- jsprojectmetacrft.js
+++ jsprojectmetacrft.js
@@ -31,7 +31,7 @@ function listNFTs () {
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-    if(holdNFT[0].id == null || holdNFT[0].name == null){
+    if(holdNFT[0].id == null || holdNFT[1].id == null || holdNFT[2].id == null){
         console.log("There are no minted NFTs.");
     }else{
         for(let i = 0; i<holdNFT.length; i++){
